63ae6159c137cb00a8fa6d38ba5b38fa29bf9851
daneos <daneos@daneos.com>
2012-12-11 14:13:19 +0100

    checks: update time, NaNs, omitting NaNs at the beginning of database

--- check_munin_rrd.php
+++ check_munin_rrd.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <?php
-	$options = getopt("hf:");
+	$options = getopt("hf:t:n::");
+	$nanlevel = "warn";
 
 	$STATE_OK=0;
 	$STATE_WARNING=1;
@@ -13,6 +14,8 @@
 		echo "Check Munin RRD databases\n";
 		echo "Options:\n";
 		echo "-f file name (required)\n";
+		echo "-t x:y, if last update was more than x secs ago, WARN, if more than y, CRIT\n";
+		echo "-n [lvl] check for NaN values between correct entries, lvl can be warn or crit (defaults to warn)\n";
 		echo "-h shows this help message\n";
 	}
 
@@ -36,7 +39,54 @@
 		return $STATE_UNKNOWN;
 	}
 
-	var_dump($rrdxml);
-
+	if(in_array('t', array_keys($options)))
+	{
+		$times = explode(':', $options['t']);
+		if((time() - $rrdxml->lastupdate) > $times[1])
+		{
+			echo "CRITICAL: Last update was at ".date("D d-m-Y H:i:s O", (int)$rrdxml->lastupdate)."\n";
+			return $STATE_CRITICAL;
+		}
+		if((time() - $rrdxml->lastupdate) > $times[0])
+		{
+			echo "WARNING: Last update was at ".date("D d-m-Y H:i:s O", (int)$rrdxml->lastupdate)."\n";
+			return $STATE_WARNING;
+		}
+	}
+	if(in_array('n', array_keys($options)))
+	{
+		if($options['n']) $nanlevel = $options['n'];
+		$nan_ok = false;
+		$nancount = 0;
+		$rracount = count($rrdxml->rra);
+		for($i = 0; $i < $rracount; $i++)
+		{
+			$rowcount = count($rrdxml->rra[$i]->database->row);
+			for($j = 0; $j < $rowcount; $j++)
+			{
+				if($rrdxml->rra[$i]->database->row[$j]->v == "NaN")
+				{
+					if($j == 0) $nan_ok = true;
+					if(!$nan_ok) $nancount++;
+				}
+				else $nan_ok = false;
+			}
+		}
+		if($nancount != 0)
+		{
+			if($nanlevel == "crit")
+			{
+				echo "CRITICAL: $nancount NaNs found.\n";
+				return $STATE_CRITICAL;
+			}
+			else
+			{
+				echo "WARNING: $nancount NaNs found.\n";
+				return $STATE_WARNING;
+			}
+		}
+	}
+	echo "OK: All RRD checks passed.\n";
+	return $STATE_OK;
 
 ?>
